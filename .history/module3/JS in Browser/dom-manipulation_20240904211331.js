bdcc2ef9a6cf281b3be9e5f294c5ade752a8dd0e
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-04 21:16:25 +0100

    dom manipulation

---
+++ .history/module3/JS in Browser/dom-manipulation_20240904211331.js
@@ -0,0 +1,15 @@
+// now with the following property i can get the corresponding selector 
+
+// to look up only for the p's inside my page 
+
+document.querySelector('p')
+
+// same for my a's
+qui
+
+document.querySelector('a')
+
+// suppose i want to return something that i know its id
+// using js i can type: 
+
+document.getElementById('heading')
