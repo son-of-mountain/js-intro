e75decf6c551311e0597fc7ea7b96ba44d1a3dd3
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2024-09-11 22:00:49 +0100

    delete all test files

---
+++ .history/module4/3-testing.intro.test_20240911215805.js
@@ -0,0 +1,6 @@
+
+const concatStr = require('./2-testing-intro.js');
+
+test('returns abcv when passed "ab" and "cv"', () => {
+    expect(concatStr("ab", "cv")).toBe("abcv");
+});
---
+++ .history/module4/3-testing.intro.test_20240911220021.js
@@ -0,0 +1,6 @@
+const {default : TestRunner} = require("jest-runner")
+const concatStr = require('./concatString.js');
+
+test('returns abcv when passed "ab" and "cv"', () => {
+    expect(concatStr("ab", "cv")).toBe("abcv");
+});
